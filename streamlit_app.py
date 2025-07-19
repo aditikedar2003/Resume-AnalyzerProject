f6d4fac89fec37a866d0d6a5b81d207aca21d9be
aditikedar2003 <aditikedar66@gmail.com>
2025-07-19 08:41:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ def connect_db():
 # --- HEADER NAVIGATION ---
 col1, col2, col3 = st.columns([1, 6, 1])
 with col1:
-    st.image("https://raw.githubusercontent.com/aditikedar2003/Resume-Analyzer-Final/main/logo.png", width=100)
+    st.image("https://raw.githubusercontent.com/aditikedar2003/Resume-Analyzer-Final/main/logo.png", width=200)
+
 with col2:
     st.markdown("""
     <style>
