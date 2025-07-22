326b9cb4a1fabc15ab02cd1da61ce404c3cc4518
aditikedar2003 <aditikedar66@gmail.com>
2025-07-22 11:25:36 +0530

    Update router.py

--- apps/backend/app/api/router.py
+++ apps/backend/app/api/router.py
@@ -1,12 +1,11 @@
-# app/api/router.py
-
 from fastapi import APIRouter
+from app.api.resume import resume_router
+from app.api.user import user_router
+from app.api.job import job_router
 
-router = APIRouter()
+v1_router = APIRouter()
 
-# ⬇️ IMPORT AFTER defining router to avoid circular import
-from app.api.endpoints import resume, user, job
-
-router.include_router(resume.router, prefix="/resume", tags=["Resume"])
-router.include_router(user.router, prefix="/user", tags=["User"])
-router.include_router(job.router, prefix="/job", tags=["Job"])
+# Prefix v1 to all routes
+v1_router.include_router(resume_router, prefix="/resume", tags=["Resume"])
+v1_router.include_router(user_router, prefix="/user", tags=["User"])
+v1_router.include_router(job_router, prefix="/job", tags=["Job"])
