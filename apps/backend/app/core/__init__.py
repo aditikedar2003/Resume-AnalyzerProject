9cadbfc031a836dd103f205327c4943517ba1872
aditikedar2003 <aditikedar66@gmail.com>
2025-07-22 10:19:35 +0530

    Update __init__.py

--- apps/backend/app/core/__init__.py
+++ apps/backend/app/core/__init__.py
@@ -1,4 +1,7 @@
+# app/core/__init__.py
+from .config import settings
 from .database import init_models, async_engine, get_db_session, get_sync_db_session
+from .logging import setup_logging
 from .exceptions import (
     custom_http_exception_handler,
     validation_exception_handler,
@@ -6,10 +9,12 @@ from .exceptions import (
 )
 
 __all__ = [
+    "settings",
     "init_models",
     "async_engine",
     "get_db_session",
     "get_sync_db_session",
+    "setup_logging",
     "custom_http_exception_handler",
     "validation_exception_handler",
     "unhandled_exception_handler",
