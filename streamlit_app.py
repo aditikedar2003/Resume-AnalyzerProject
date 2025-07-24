621b0d34d9341bcbf2c324b35dbe5d846b92c6c0
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-24 14:54:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
+import streamlit as st
+from views.auth import show_login_page, show_signup_page
+from views.resume_scanner import show_resume_scanner
+from views.cover_letter import show_cover_letter_analyzer
+from views.linkedin import show_linkedin_optimizer
+from views.job_tracker import show_job_tracker
+from views.resume_builder import show_resume_builder
+from views.dashboard import show_dashboard
+from views.navbar import show_navbar
+
+from utils.session import get_user_id  # ✅ THIS LINE FIXES THE ERROR
 from views.auth import show_login_page, show_signup_page
 # ...
 
