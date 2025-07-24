6e3e726d19a79f588d18bfc35627b74f2ffda50e
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-24 17:39:18 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,3 +18,28 @@ def get_db_connection():
     conn = connect()
     cur = conn.cursor()
     return conn, cur
+
+# -----------------------
+# Auth-related functions
+# -----------------------
+
+def get_user_by_email(email):
+    conn, cur = get_db_connection()
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user
+
+def insert_user(name, email, password_hash):
+    conn, cur = get_db_connection()
+    cur.execute("""
+        INSERT INTO users (name, email, password)
+        VALUES (%s, %s, %s)
+        RETURNING id
+    """, (name, email, password_hash))
+    user_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return user_id
