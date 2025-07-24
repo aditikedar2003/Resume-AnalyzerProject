9b0aab6072f8a4d32be387674a0312ffe86e8e39
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-24 21:34:56 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,21 +1,33 @@
 import psycopg2
-import psycopg2.extras
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        cursor_factory=psycopg2.extras.RealDictCursor
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
     )
+    return conn
 
+def get_user_by_email(email):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user
 
 def insert_user(full_name, email, password_hash):
     conn = get_db_connection()
@@ -25,16 +37,8 @@ def insert_user(full_name, email, password_hash):
         VALUES (%s, %s, %s)
         RETURNING id
     """, (full_name, email, password_hash))
-    user_id = cur.fetchone()['id']
+    user_id = cur.fetchone()[0]
     conn.commit()
+    cur.close()
     conn.close()
     return user_id
-
-
-def get_user_by_email(email):
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user = cur.fetchone()
-    conn.close()
-    return user
