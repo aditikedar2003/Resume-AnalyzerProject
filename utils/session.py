4b9303631e344bcaa7e1b9f0ae3f0d80d189c2a9
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-25 11:50:37 +0530

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
-def is_authenticated():
-    return "user_id" in st.session_state and st.session_state["user_id"]
+def get_current_user_id():
+    return st.session_state.get("user_id", None)
 
-def set_user_id(uid):
-    st.session_state["user_id"] = uid
+def set_current_user(user_id, user_name, user_email):
+    st.session_state["user_id"] = user_id
+    st.session_state["user_name"] = user_name
+    st.session_state["user_email"] = user_email
 
-def logout():
-    st.session_state.pop("user_id", None)
+def clear_session():
+    for key in ["user_id", "user_name", "user_email"]:
+        if key in st.session_state:
+            del st.session_state[key]
