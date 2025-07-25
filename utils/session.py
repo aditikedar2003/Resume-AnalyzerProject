be5118934b41ffe72367f74f12aa3db12ee0d31d
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-25 13:55:58 +0530

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 def set_user_id(user_id):
-    st.session_state.user_id = user_id
+    st.session_state['user_id'] = user_id
 
 def get_user_id():
-    return st.session_state.get("user_id", None)
-
-def logout_user():
-    st.session_state.user_id = None
+    return st.session_state.get('user_id', None)
