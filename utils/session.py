72471e8b683cbe8d867efe9d10370e630116b453
Aditi Ramchandra Kedar <aditikedar66@gmail.com>
2025-07-25 12:55:12 +0530

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,14 +1,10 @@
 import streamlit as st
 
-def get_current_user_id():
+def set_user_id(user_id):
+    st.session_state.user_id = user_id
+
+def get_user_id():
     return st.session_state.get("user_id", None)
 
-def set_current_user(user_id, user_name, user_email):
-    st.session_state["user_id"] = user_id
-    st.session_state["user_name"] = user_name
-    st.session_state["user_email"] = user_email
-
-def clear_session():
-    for key in ["user_id", "user_name", "user_email"]:
-        if key in st.session_state:
-            del st.session_state[key]
+def logout_user():
+    st.session_state.user_id = None
